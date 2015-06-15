dc668d25d3849450498ea4b3bc46ab866a5bc525
Anders Lundgren <lundgren.am@gmail.com>
2015-06-14 22:34:32 -0400

    Fixed menu animation queueing

--- js/menu.js
+++ js/menu.js
@@ -8,14 +8,14 @@ jQuery(function($) {
         if (!$target.hasClass('active')) {
             $other.each(function(index, self) {
                 var $this = $(this);
-                $this.removeClass('active').animate({
-                    left: $this.width()
+                $this.removeClass('active').stop().animate({
+                    left: $this.width(),
                 }, 500);
             });
 
             $target.addClass('active').show().css({
-                left: -($target.width())
-            }).animate({ left: 0 }, 500);
+                left: -($target.width()),
+            }).stop().animate({ left: 0 }, 500);
         }
     });
 
