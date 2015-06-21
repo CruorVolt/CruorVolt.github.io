52b469893a8e16ecc134f0da8e85a17470f4041f
Anders Lundgren <lundgren.am@gmail.com>
2015-06-21 11:35:45 -0400

    Prettier - almost ready for actual content.

--- js/menu.js
+++ js/menu.js
@@ -2,19 +2,15 @@ jQuery(function($) {
 
     $('a.panel').click(function() {
 
-
         var $target = $($(this).attr('href')),
             $other = $target.siblings('.active');
         
         if (!$target.hasClass('active')) {
-	    $(this).find("img").addClass('border');
             $other.each(function(index, self) {
                 var $this = $(this);
                 $this.removeClass('active').stop().animate({
                     left: $this.width(),
                 }, 500);
-		$this.find("img").removeClass('border');
-		console.log("REMOVING");
             });
 
             $target.addClass('active').show().css({
