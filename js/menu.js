c112830f1c88ce9b5512c46eb8e94234be6aac2f
Anders Lundgren <lundgren.am@gmail.com>
2015-07-04 09:59:24 -0400

    Someday the 303 iframe will animate. Someday.

--- js/menu.js
+++ js/menu.js
@@ -19,8 +19,18 @@ jQuery(function($) {
         }
     });
 
+
+    var src = $('img#life_img').attr('src');
+    $('img#life_img').hover(function() {
+        $(this).attr('src', src.replace('img/shuttle_static.gif', 'img/shuttle.gif'));
+    }, function(){
+      $(this).attr('src', src);
+    });
+
+
 });
 
 $(document).ready(function() {
 	$("div#left").find("a#defaultPanel").trigger('click');
 });
+
