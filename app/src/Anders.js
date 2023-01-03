68a5dea9042ff951aa5ebb8e50871c73ec3e0bf2
Anders <3857181+CruorVolt@users.noreply.github.com>
2023-01-02 23:13:01 -0700

    New gif for soundcloud

--- app/src/Anders.js
+++ app/src/Anders.js
@@ -4,7 +4,9 @@ import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faSoundcloud, faRedditAlien } from '@fortawesome/free-brands-svg-icons'
 import { faCarrot, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
-import gosper from './gosper.gif';
+
+import shuttle from './shuttle.gif';
+import soundCloud from './soundcloud.gif';
 import RedditMatrix from './reddit_matrix';
 
 
@@ -31,33 +33,39 @@ function Anders() {
                 <RedditMatrix/>
               </div>
             )}}
-            onMouseLeave={ () => {setHoverPane(null)}}
           />
 
           <FontAwesomeIcon 
             className='icon life' 
             icon={faPaperPlane} 
-            onClick={() => { window.open('https://github.com/CruorVolt', '_blank'); }} 
+            onClick={() => { window.open('https://github.com/CruorVolt/life', '_blank'); }} 
             onMouseEnter={ () => {setHoverPane( 
               <div className='life-box'>
-                <img src={gosper} height={'200px'}/>
+                <img src={shuttle} alt='life' height={'200px'}/>
               </div>
             )}}
-            onMouseLeave={ () => {setHoverPane(null)}}
           />
 
           <FontAwesomeIcon 
             className='icon' 
             icon={faGithub} 
             onClick={() => { window.open('https://github.com/CruorVolt', '_blank'); }} 
-            onMouseLeave={ () => {setHoverPane(null)}}
+            onMouseEnter={ () => {setHoverPane( 
+              <div className='reddit-box'>
+                <RedditMatrix/>
+              </div>
+            )}}
           />
 
           <FontAwesomeIcon 
             className='icon' 
             icon={faSoundcloud} 
             onClick={() => { window.open('https://soundcloud.com/anders-lundgren-589704197', '_blank'); }} 
-            onMouseLeave={ () => {setHoverPane(null)}}
+            onMouseEnter={ () => {setHoverPane( 
+              <div className='life-box'>
+                <img src={soundCloud} alt='soundcloud' height={'200px'}/>
+              </div>
+            )}}
           />
 
         </div>
